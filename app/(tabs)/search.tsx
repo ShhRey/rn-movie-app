20292f9ee7892cfb415350d4b0e83ed17b593cce
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-03-17 16:11:32 -0400

    Setting up Routes, Navigation using Tabs

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Search = () => {
+  return (
+    <View>
+      <Text>Search for Movies</Text>
+    </View>
+  )
+}
+
+export default Search
