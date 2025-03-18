d748be8f6bac41cae71082bf8019de6bd6258b58
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-03-18 16:45:34 -0400

    Dynamically render cards displaying movie info for Home Page

---
+++ components/MovieCard.tsx
@@ -0,0 +1,25 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie) => {
+	return (
+		<Link href={`/movies/${id}`} asChild>
+			<TouchableOpacity className='w-[30%]'>
+				<Image 
+					className='w-full h-52 rounded-lg'
+					resizeMode='cover'
+					source={{
+						uri: poster_path 
+							? `https://image.tmdb.org/t/p/w500${poster_path}`
+							: `https://placehold.co/600x400/1a1a1a/FFFFFF.png`
+					}} 
+				/>
+
+				<Text className='text-sm font-bold text-white mt-2'>{title}</Text>
+			</TouchableOpacity>
+		</Link>
+	)
+}
+
+export default MovieCard
