64761f3562b5413b0ffd2e6c9d88782108a516e9
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-03-19 11:56:26 -0400

    Hiding/Showing Statusbar info

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Stack } from "expo-router";
 import './globals.css';
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-	return <Stack>
-		{/* Hiding Default Header for Group Route */}
-		<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-		<Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
-	</Stack>
+	return (
+		<>
+			{/* Hiding Time and Battery from Statusbar */}
+			{/* <StatusBar hidden={true} /> */}
+			<Stack>
+				{/* Hiding Default Header for Group Route */}
+				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+				<Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+			</Stack>
+		</>
+	)
 }
